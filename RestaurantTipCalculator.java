104cfb62cbb927ee687b5012ab28ac2b21f394fa
BT_2N2_15 <sample@bths.edu>
2023-09-21 13:27:53 -0400

    initial commit

---
+++ RestaurantTipCalculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class RestaurantTipCalculator {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the tip calculator!");
+        System.out.print("How many people are in your group: ");
+
+
+
+
+    }
+}
