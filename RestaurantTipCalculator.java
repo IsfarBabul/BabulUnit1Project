732444888679a4d4747a2c79af353c5d4c746926
BT_2N2_15 <sample@bths.edu>
2023-09-21 13:31:42 -0400

    initial commit

--- RestaurantTipCalculator.java
+++ RestaurantTipCalculator.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 public class RestaurantTipCalculator {
     public static void main(String[] args) {
 
+        int numPeople;
+        int tipPercentage;
+        int americanState;
+        int totalPrice;
+
         System.out.println("Welcome to the tip calculator!");
         System.out.print("How many people are in your group: ");
+        numPeople = scan.nextInt();
 
 
 
