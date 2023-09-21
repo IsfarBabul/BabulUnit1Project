942e86b83e4b55b6b848977a1911d509df1c2774
BT_2N2_15 <sample@bths.edu>
2023-09-21 13:38:41 -0400

    questions asked

--- RestaurantTipCalculator.java
+++ RestaurantTipCalculator.java
@@ -3,12 +3,20 @@ import java.util.Scanner;
 public class RestaurantTipCalculator {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
         int numPeople;
         int tipPercentage;
         int americanState;
         int totalPrice;
 
         System.out.println("Welcome to the tip calculator!");
+        System.out.print("How many people are in your group: ");
+        numPeople = scan.nextInt();
+        System.out.print("What's the tip percentage? (0-100): ");
+        numPeople = scan.nextInt();
+        System.out.print("What U.S.state are you in (if your not in a U.S. state type anything else)? ");
+        numPeople = scan.nextInt();
         System.out.print("How many people are in your group: ");
         numPeople = scan.nextInt();
 
