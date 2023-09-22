6b8ee9b238b7a9c0bc4f5162d4983608707f688a
Isfar Babul <isfarb2@nycstudents.net>
2023-09-21 20:36:18 -0400

    getting the basic questions down

--- RestaurantTipCalculator.java
+++ RestaurantTipCalculator.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class RestaurantTipCalculator {
     public static void main(String[] args) {
@@ -7,18 +8,33 @@ public class RestaurantTipCalculator {
 
         int numPeople;
         int tipPercentage;
+        int couponPercentage;
         State americanState;
         int totalPrice;
 
         System.out.println("Welcome to the tip calculator!");
+        //number of people
         System.out.print("How many people are in your group: ");
         numPeople = scan.nextInt();
-        System.out.print("What U.S.state are you in (if your not in a U.S. state type anything else)? ");
+        /* which U.S. state or territory your dining in
+        (used to calculate sales tax and is applied after coupons and before tips) */
+        System.out.print("What U.S. state or territory are you in? ");
         americanState = scan.nextLine();
+        americanState = americanState.toLowerCase();
+        //enter tip percentage (this is applied after coupons and sales tax)
         System.out.print("What's the tip percentage? (0-100): ");
         tipPercentage = scan.nextInt();
+        while (tipPercentage < 0 && tipPercentage > 100) {
+            System.out.print("That's not a valid tip percentage. Try again: ");
+            tipPercentage = scan.nextInt();
+        }
+        //enter coupon percentage (this is applied before any sales tax or tips)
         System.out.print("What's the coupon percentage? (0-50): ");
         numPeople = scan.nextInt();
+        while (tipPercentage < 0 && tipPercentage > 50) {
+            System.out.print("That's not a valid coupon percentage. Try again: ");
+            couponPercentage = scan.nextInt();
+        }
 
 
 
